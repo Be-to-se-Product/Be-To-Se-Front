219e3aba235537a44d14f9ccfe033d644aebca07
PedroRCSilva <pedro.rsilva@sptech.school>
2023-11-25 12:35:50 -0300

    adicionando redirecionamento para a tela inicial e criando etapa falando que o cadastro fou concluido

--- src/pages/CadastroUsuario/CadastroUsuario.jsx
+++ src/pages/CadastroUsuario/CadastroUsuario.jsx
@@ -7,84 +7,90 @@ import api from "../../services/api";
 
 import Step1 from "./componentes/Step1";
 import Step2 from "./componentes/Step2";
-import { removerMascaraCpf, removerMascaraTelefone } from "../../utils/formatadores";
+import CircularProgress from "@mui/material/CircularProgress";
+import {
+  removerMascaraCpf,
+  removerMascaraTelefone,
+} from "../../utils/formatadores";
 import { criptografar } from "../../utils/Autheticated";
+import { useNavigate } from "react-router-dom";
 
 const CadastroUsuario = () => {
-  const [stateAtual, setStateAtual] = useState(1);
+  const [stateAtual, setStateAtual] = useState(0);
   const [data, setData] = useState({});
 
+  const navigate = useNavigate();
+
   const avancar = (data) => {
-    setData(prevState => ({ ...prevState, ...data }));
-    if(stateAtual<steps.length-2){
-      setStateAtual(stateAtual + 1)
-    return
-    };
+    setData((prevState) => ({ ...prevState, ...data }));
+    if (stateAtual < steps.length - 2) {
+      setStateAtual(stateAtual + 1);
+      return;
+    }
     saveUser(data);
   };
 
   const saveUser = (dataFunction) => {
-  
-
     const dataMerge = { ...data, ...dataFunction };
-
 
     const dataRequest = {
       nome: dataMerge.nome,
       cpf: removerMascaraCpf(dataMerge.cpf),
-      celular:  removerMascaraTelefone(dataMerge.telefone),
+      celular: removerMascaraTelefone(dataMerge.telefone),
       dataNascimento: dataMerge.dtNascimento,
       genero: dataMerge.genero,
-      usuario:{
+      usuario: {
         email: dataMerge.email,
         senha: dataMerge.senha,
-      }
-     
+      },
     };
 
-    api.post("/consumidores", dataRequest)
-    .then((response) => {
-    if(response.status==201){
-      sessionStorage.setItem("userDetails",criptografar(response.data))
-      setStateAtual(stateAtual + 1)
-    };
+    sessionStorage.clear();
 
-    }
-    ).catch((error) => {
-      console.log(error);
-    })
-  }
+    api
+      .post("/consumidores", dataRequest)
+      .then((response) => {
+        if (response.status == 201) {
+          sessionStorage.setItem("USERDETAILS", criptografar(response.data));
+          setStateAtual(stateAtual + 1);
+
+          setTimeout(() => {
+            navigate("/index");
+          }, 3000);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const retroceder = () => {
     setStateAtual(stateAtual - 1);
   };
 
- 
-
   const steps = [
     <Step1 getDataForm={avancar} data={data}>
       <div className="flex w-10/12 mx-auto justify-center mt-8 gap-x-5">
-        
-        
         <Button>Avançar</Button>
       </div>
     </Step1>,
-    <Step2 getDataForm={avancar}  data={data}>
+    <Step2 getDataForm={avancar} data={data}>
       <div className="flex w-10/12 mx-auto justify-center mt-8 gap-x-5">
         <Button type="button" onClick={retroceder}>
           Retroceder
         </Button>
         <Button>Cadastrar</Button>
       </div>
-    </Step2>
-    ,
-    <div className="flex w-10/12 mx-auto justify-center mt-8 gap-x-5">
-      Agendamento concluido
-      Redirecionando para a tela incial
-    </div>
-    ,
+    </Step2>,
+    <div className="flex flex-col w-9/12 mx-auto justify-center items-center mt-20 gap-y-5 bg-white-principal py-20 rounded">
+      <img src="/src/assets/selection.png" alt="" className="w-44" />
+      <div className="text-center flex flex-col gap-y-2">
+        <h2 className="text-xl">Cadastrado concluído com sucesso!</h2>
+        <p>Você sera redirecionado para a tela inicial</p>
+        <CircularProgress className="mx-auto" />
+      </div>
+    </div>,
   ];
-
 
   return (
     <>
@@ -96,10 +102,14 @@ const CadastroUsuario = () => {
 
       <main className="w-full h-[89vh] bg-black-200 flex justify-center ">
         <div className={`w-2/5  px-10 py-10 `}>
-          <div className="flex flex-col gap-y-4">
-            <h2 className="text-center text-2xl">Informações do usuário</h2>
+          <div className={`flex flex-col gap-y-4 `}>
+            <h2
+              className={`text-center text-2xl ${stateAtual == 2 && "hidden"}`}
+            >
+              Informações do usuário
+            </h2>
 
-            <div className={`w-5/12 mx-auto `}>
+            <div className={`w-5/12 mx-auto ${stateAtual == 2 && "hidden"}`}>
               <StepperRoot.Content>
                 <StepperRoot.Step number={1} stateAtual={stateAtual}>
                   <div className="text-center">
