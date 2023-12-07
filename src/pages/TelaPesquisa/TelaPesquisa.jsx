aa4ac458c6fd3c94aee8158de97935c82006ac0e
cesarbmartins <cesar.martins@sptech.school>
2023-12-06 22:53:40 -0300

    Ajustando filtros

--- src/pages/TelaPesquisa/TelaPesquisa.jsx
+++ src/pages/TelaPesquisa/TelaPesquisa.jsx
@@ -11,147 +11,155 @@ import DistanceFilter from "./componentes/DistanceFilter";
 import { useLocation } from "react-router-dom";
 
 function TelaPesquisa(props) {
-    const navigate = useNavigate();
-    const { nomePesquisado } = location.state || {};
+  const navigate = useNavigate();
+  const { nomePesquisado } = location.state || {};
 
-    const [originCoordinates, setOriginCoordinates] = useState({ lat: null, lon: null });
-    const [produtos, setProdutos] = useState([]);
-    //filtros
-    const [metodosPagamento, setMetodosPagamento] = useState([]);
-    const [nome, setNome] = useState(nomePesquisado ? nomePesquisado : null);
-    const [metodoPagamento, setMetodoPagamento] = useState();
-    const [distancia, setDistancia] = useState(50);
+  const [originCoordinates, setOriginCoordinates] = useState({
+    lat: null,
+    lon: null,
+  });
+  const [produtos, setProdutos] = useState([]);
+  //filtros
+  const [metodosPagamento, setMetodosPagamento] = useState([]);
+  const [nome, setNome] = useState(nomePesquisado ? nomePesquisado : null);
+  const [metodoPagamento, setMetodoPagamento] = useState();
+  const [distancia, setDistancia] = useState(50);
 
-    const [mostrarMapa, setMostrarMapa] = useState(false);
+  const [mostrarMapa, setMostrarMapa] = useState(false);
 
-    const handleVerNoMapa = () => {
-        setMostrarMapa(!mostrarMapa);
-    };
+  const handleVerNoMapa = () => {
+    setMostrarMapa(!mostrarMapa);
+  };
 
-    useEffect(() => {
-        if (mostrarMapa) {
-            navigate("/mapa", {
-                state: {
-                    nome,
-                    distancia,
-                    metodoPagamento,
-                },
-            });
-        }
-    }, [mostrarMapa]);
-
-
-    useEffect(() => {
-        geolocation(setOriginCoordinates);
+  useEffect(() => {
+    if (mostrarMapa) {
+      navigate("/mapa", {
+        state: {
+          nome,
+          distancia,
+          metodoPagamento,
+        },
+      });
     }
-        , []);
+  }, [mostrarMapa]);
 
-    const handleNomeChange = (novoNome) => {
-        setNome(novoNome);
-    };
+  useEffect(() => {
+    geolocation(setOriginCoordinates);
+  }, []);
 
-    useEffect(() => {
-        toast.loading("Carregando produtos ...");
+  const handleNomeChange = (novoNome) => {
+    setNome(novoNome);
+  };
 
-        api.get("/produtos/mapa", {
-            params: {
-                latitude: originCoordinates.lat,
-                longitude: originCoordinates.lon,
-                distancia: distancia,
-                nome: nome,
-                metodoPagamento: metodoPagamento,
-            },
-        }).then((response) => {
-            setProdutos(response.data ? response.data : []);
-        }).catch((error) => {
-            console.log(error);
-        }).finally(() => {
-            toast.dismiss()
-        })
-    }
-        , [originCoordinates, nome, metodoPagamento, distancia]);
+  useEffect(() => {
+    toast.loading("Carregando produtos ...");
 
+    api
+      .get("/produtos/mapa", {
+        params: {
+          latitude: originCoordinates.lat,
+          longitude: originCoordinates.lon,
+          distancia: distancia,
+          nome: nome,
+          metodoPagamento: metodoPagamento,
+        },
+      })
+      .then((response) => {
+        setProdutos(response.data ? response.data : []);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        toast.dismiss();
+      });
+  }, [originCoordinates, nome, metodoPagamento, distancia]);
 
-    useEffect(() => {
-        getMetodosPagamento();
-    }, []);
+  useEffect(() => {
+    getMetodosPagamento();
+  }, []);
 
-    const getMetodosPagamento = () => {
-        api.get("/metodos-pagamento")
-            .then((response) => {
-                setMetodosPagamento(response.data ? response.data : []);
-            }).catch((error) => {
-                console.log(error);
-            });
-    };
+  const getMetodosPagamento = () => {
+    api
+      .get("/metodos-pagamento")
+      .then((response) => {
+        setMetodosPagamento(response.data ? response.data : []);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    const handleMetodoChange = (event) => {
-        setMetodoPagamento(event.target.value);
-    }
+  const handleMetodoChange = (event) => {
+    setMetodoPagamento(event.target.value);
+  };
 
-    return (
-        <div >
-            <NavbarRoot.Content>
-                <NavbarRoot.ContentTop>
-                    <NavbarRoot.Logo />
-                    <NavbarRoot.Pesquisa onChange={handleNomeChange} />
-                    {sessionStorage.USERDETAILS ? (<NavbarRoot.Authenticated />) : (<NavbarRoot.Sign />)}
+  return (
+    <div>
+      <NavbarRoot.Content>
+        <NavbarRoot.ContentTop>
+          <NavbarRoot.Logo />
+          <NavbarRoot.Pesquisa onChange={handleNomeChange} />
+          {sessionStorage.USERDETAILS ? (
+            <NavbarRoot.Authenticated />
+          ) : (
+            <NavbarRoot.Sign />
+          )}
+        </NavbarRoot.ContentTop>
+        <NavbarRoot.Menu>
+          <NavbarRoot.Item></NavbarRoot.Item>
+        </NavbarRoot.Menu>
+      </NavbarRoot.Content>
 
-                </NavbarRoot.ContentTop>
-                <NavbarRoot.Menu>
-                    <NavbarRoot.Item></NavbarRoot.Item>
-                </NavbarRoot.Menu>
-            </NavbarRoot.Content>
+      <main className="flex pt-[48px] flex-col items-center gap-[48px]">
+        <main className="flex w-auto flex-col items-center gap-[48px]">
+          <div className="flex flex-row w-full justify-between items-center ">
+            <div className="flex w-[715px] h-[39px] gap-6 pl-4">
+              <div className="flex flex-col">
+                <label className="text-sm font-semibold mb-1">
+                  Método de Pagamento:
+                </label>
+                <Select
+                  className="flex p-4 items-center gap-8 w-60 h-3 bg-white-principal"
+                  onClick={getMetodosPagamento}
+                  onChange={handleMetodoChange}
+                  value={metodoPagamento ? metodoPagamento : []}
+                >
+                  {metodosPagamento?.map((metodo) => (
+                    <MenuItem key={metodo.id} value={metodo.descricao}>
+                      {metodo.descricao}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </div>
+              <div className="flex flex-col">
+                <label className="text-sm font-semibold mb-1">Distância:</label>
+                <DistanceFilter onChange={(filter) => setDistancia(filter)} />
+              </div>
+            </div>
+            <div className="flex w-[200px] justify-end items-center gap-3">
+              <p className="" onClick={handleVerNoMapa}>
+                Ver no mapa
+              </p>
+              <Switch onClick={handleVerNoMapa}></Switch>
+            </div>
+          </div>
 
-            <main className="flex pt-[48px] flex-col items-center gap-[48px]">
-                <main className="flex w-auto flex-col items-center gap-[48px]">
-                    <div className="flex flex-row w-full justify-between items-center ">
-                        <div className="flex w-[715px] h-[39px] gap-10 pl-4">
-                            <div className="flex pt-2 pr-2 pb-2 pl-2 p-10 items-center border-2 gap-4">
-                                <Select
-                                    className="flex p-4 items-center gap-8 w-full bg-white-principal"
-                                    onClick={getMetodosPagamento}
-                                    onChange={handleMetodoChange}
-                                    value={metodoPagamento ? metodoPagamento : []}
-                                >
-                                    {metodosPagamento?.map((metodo) => (
-                                        <MenuItem key={metodo.id} value={metodo.descricao}>
-                                            {metodo.descricao}
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </div>
-                            <div className="flex pt-4 pr-2 pb-2 pl-2 items-center border-2 gap-4">
-                                <DistanceFilter onChange={(filter) => setDistancia(filter)} />
-                            </div>
-                        </div>
-                        <div className="flex w-[200px] justify-end items-center gap-3">
-                            <p className="" onClick={handleVerNoMapa}>
-                                Ver no mapa
-                            </p>
-                            <Switch onClick={handleVerNoMapa}></Switch>
-                        </div>
-                    </div>
+          <div className="flex w-full gap-10 items-center pl-4">
+            <h2 className="text-xl">Resultados da pesquisa:</h2>
+            <p className="text-xl">{`${nome ? nome : ""}`}</p>
+          </div>
 
-                    <div className="flex w-full gap-10 items-center pl-4">
-                        <h2 className="text-xl">
-                            Resultados da pesquisa:
-                        </h2>
-                        <p className="text-xl">
-                            {`${nome ? nome : ''}`}
-                        </p>
-                    </div>
-
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-                        {produtos?.map((produto) => (
-                            <CardProdutoPesquisa key={produto?.id} produto={produto} />
-                        ))}
-                    </div>
-                </main>
-            </main>
-            <ToastContainer></ToastContainer>
-        </div>
-    )
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+            {produtos?.map((produto) => (
+              <CardProdutoPesquisa key={produto?.id} produto={produto} />
+            ))}
+          </div>
+        </main>
+      </main>
+      <ToastContainer></ToastContainer>
+    </div>
+  );
 }
 
 export default TelaPesquisa;
