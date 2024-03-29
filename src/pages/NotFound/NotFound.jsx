0d8b37a0b8d58fbae6a4463eab38d89d0787cf09
PedroRCSilva <pedro.rsilva@sptech.school>
2024-03-28 21:12:23 -0300

    att

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -2,8 +2,9 @@ import PictureNotFound from "@assets/NotFound.png"
 
 const NotFound = () => {
   return (
-    
+    <>  {":)"}
    <img src={PictureNotFound} alt="" className='w-10/12 h-screen object-contain mx-auto' />
+   </>
   )
 }
 
