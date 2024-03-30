f62017d48b69b4cb5d929f1e66b092725e9f5c3a
PedroRCSilva <pedro.rsilva@sptech.school>
2024-03-29 22:30:53 -0300

    delete string

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -2,9 +2,7 @@ import PictureNotFound from "@assets/NotFound.png"
 
 const NotFound = () => {
   return (
-    <>  {":)"}
    <img src={PictureNotFound} alt="" className='w-10/12 h-screen object-contain mx-auto' />
-   </>
   )
 }
 
